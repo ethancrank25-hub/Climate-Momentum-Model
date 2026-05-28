e67164e94340a20a32c2e32adf6f69269831ca82
ethancrank25-hub <ethancrank25@gmail.com>
2026-05-28 12:20:23 -0400

    Update data_prep.py

--- scripts/data_prep.py
+++ scripts/data_prep.py
@@ -1,10 +1,20 @@
-scripts/data_prep.py
 import requests
+
 print("Starting NOAA data prep...")
+
 url = "https://www.ncdc.noaa.gov/cdo-web/api/v2/datasets"
+
 headers = {
-  "token": "YOUR_NOAA_TOKEN"
+    "token": "YOUR_NOAA_TOKEN"
 }
-response = request.get(url, headers=headers)
+
+print("Sending request...")
+
+response = requests.get(url, headers=headers)
+
+print("Request finished")
+
 print("Status code:", response.status_code)
-print(response.text[:200])
+
+print("Raw response:")
+print(response.text)
